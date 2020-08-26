9a00ddac8e3a5cf245e789eb9003a0d0111c6cdc
rainavyas <sayvaniar@gmail.com>
2020-08-26 12:38:20 +0100

    add a get_ID function

--- get_URL.py
+++ get_URL.py
@@ -24,3 +24,20 @@ def get_summary(url, season):
     player = split_url.pop()
     split_url.extend(["matchlogs", season, "summary", player + "-Match-Logs"])
     return("/".join(split_url))
+
+def get_ID(player_name):
+    driver = webdriver.Chrome()
+    driver.get("https://fbref.com/en")
+    time.sleep(1)
+
+    #team_names = driver.find_elements_by_xpath("//a[@name='Liverpool']")
+
+    search_bar = driver.find_element_by_name("search")
+    search_bar.send_keys(player_name)
+    time.sleep(1)
+    driver.find_element_by_class_name("ac-suggestion").click()
+    time.sleep(1)
+    url = driver.current_url
+    url_parts = url.split('/')
+    id = url_parts[5]
+    return id
