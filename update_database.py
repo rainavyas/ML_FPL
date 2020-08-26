e5870297407850fc81b918aceeb3c4630bbea25b
rainavyas <sayvaniar@gmail.com>
2020-08-26 01:17:10 +0100

    pwd local database server

--- update_database.py
+++ update_database.py
@@ -25,7 +25,7 @@ def get_table_data(url):
     return table_data
 
 def create_tables():
-    mydb = mysql.connector.connect(host='localhost',database='mysql',user='root',password="499587li")
+    mydb = mysql.connector.connect(host='localhost',database='mysql',user='root',password="abc314")
     mycursor = mydb.cursor()
 
     mycursor.execute("DROP DATABASE FPL")
@@ -37,7 +37,7 @@ def create_tables():
     mycursor.execute("CREATE TABLE Playermatches (Playermatch_id VARCHAR(255) PRIMARY KEY, Player_id INT NOT NULL, Match_id INT NOT NULL, Start VARCHAR(255), Min INT, Gls INT, Ast INT, PK INT, PKA INT, Sh INT, SoT INT, CrdY INT, CrdR INT, Tch INT, Prs INT, Tkl INT, Intr INT, Blc INT, xG FLOAT, npxG FLOAT, xA Float, SCA INT, GCA INT, cmp INT, Attp INT, PrgD INT, SuccD INT, AttD INT, FOREIGN KEY(Player_id) REFERENCES Players(Player_id), FOREIGN KEY(Match_id) REFERENCES Matches(Match_id))")
 
 def populate_tables(Name, Season, season_table):
-    mydb = mysql.connector.connect(host='localhost',database='FPL',user='root',password="499587li")
+    mydb = mysql.connector.connect(host='localhost',database='FPL',user='root',password="abc314")
     mycursor = mydb.cursor()
 
     Team = season_table[0]['Squad']
@@ -61,18 +61,3 @@ def populate_tables(Name, Season, season_table):
         mycursor.execute(f"INSERT INTO Playermatches (Playermatch_id, Player_id, Match_id, Start, Min, Gls, Ast, PK, PKA, Sh, SoT, CrdY, CrdR, Tch, Prs, Tkl, Intr, Blc, xG, npxG, xA, SCA, GCA, cmp, Attp, PrgD, SuccD, AttD) VALUES ('{str(player_id)+'-'+str(match_id)}', '{player_id}', '{match_id}', '{g['Start']}', '{g['Min']}', '{g['Gls']}', '{g['Ast']}', '{g['PK']}', '{g['PKatt']}', '{g['Sh']}', '{g['SoT']}', '{g['CrdY']}', '{g['CrdR']}', '{g['Touches']}', '{g['Press']}', '{g['Tkl']}', '{g['Int']}', '{g['Blocks']}', '{g['xG']}', '{g['npxG']}', '{g['xA']}', '{g['SCA']}', '{g['GCA']}', '{g['Cmp']}', '{g['Att']}', '{g['PrgDist']}', '{g['Carries']}', '{g['Succ']}')")
 
     mydb.commit()
-
-    #mycursor.execute("INSERT INTO Players (Name, Team) ")
-create_tables()
-
-url = "https://fbref.com/en/players/e06683ca/matchlogs/2019-2020/summary/Virgil-van-Dijk-Match-Logs"
-table_data = get_table_data(url)
-clean_table = [i for i in table_data if i["Comp"] == "Premier League"]
-populate_tables("Van Dijk", "2019-2020", clean_table)
-
-#mydb = mysql.connector.connect(host='localhost',database='mysql',user='root',password="499587li")
-#mycursor = mydb.cursor()
-
-#mycursor.execute("USE test")
-#mycursor.execute("INSERT INTO Players VALUES (1, 'Carl', 'Adian', 4, 3)")
-#mydb.commit()
